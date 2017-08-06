81aa9c9fd0b24dc3c6cd561b630d6478cd463ee5
Oscar J Delgado <lijantropique@protonmail.com>
2017-08-06 09:51:20 -0400

    Add lowercase function letter and string & count letters

--- CiO/home/most_wanted_letter.hs
+++ CiO/home/most_wanted_letter.hs
@@ -1,2 +1,11 @@
 -- The most wanted letter
 -- htpps://py.checkio.org/mission/most-wanted-letter/
+
+
+lowerL letter = if letter `elem` ['A'..'Z']
+                then head [snd item | item <- zip ['A'..'Z']['a'..'z'], letter == fst item]
+                else letter
+lowerS string = [lowerL x | x <- string, x `elem` ['a'..'z']++['A'..'Z']]
+count string letter = sum[1 | x <- string, x==letter]
+
+checkio string = snd (maximum[(count string x,x)| x <- lowerS string])
