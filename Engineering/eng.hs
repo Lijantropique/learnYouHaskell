76a8f28df8fef9d81d3e6442e923529a1a243573
Lijantropique <lijantropique@protonmail.com>
2017-08-17 16:01:05 -0500

    Remove units from basic functions

--- Engineering/eng.hs
+++ Engineering/eng.hs
@@ -7,29 +7,26 @@ Project: Process Engineering Calculations with Haskell
 
 unitsCheck :: (RealFloat a) => (String, Int) -> a
 unitsCheck (units, group)
-    |not (units `elem` [fst x|x<-(validU!!group)])   = error "Verify flow units"
-    |otherwise = head [snd x | x <- (validU!!group), fst x == units]
-    where
-        flowU = [("gpm",6.3090199463E-5),("MGD",4.3812638516E-5),("lpm",1/6000),("m3/s",1)]
-        lengthU = [("in",0.3048/12),("ft",0.3048),("mm",0.001),("m",1)]
-        fluxU = [("GFD",4.371595316108E-7),("LMH",2.7777778-7),("m/s",1)]
-        validU = [flowU, lengthU, fluxU]
-        -- Add list with error messages
+|not (units `elem` [fst x|x<-(validU!!group)])   = error "Verify flow units"
+|otherwise = head [snd x | x <- (validU!!group), fst x == units]
+where
+    flowU = [("gpm",6.3090199463E-5),("MGD",4.3812638516E-5),("lpm",1/6000),("m3/s",1)]
+    lengthU = [("in",0.3048/12),("ft",0.3048),("mm",0.001),("m",1)]
+    fluxU = [("GFD",4.371595316108E-7),("LMH",2.7777778-7),("m/s",1)]
+    validU = [flowU, lengthU, fluxU]
+    -- Add list with error messages
 
-area :: (RealFloat a) => (a, String) -> a
-area (diam, "")  = error "Include units"
-area (diam, units)
+-- Basic assumption: all units are SI.
+
+
+area :: (RealFloat a) => a -> a
+-- area (diam, "")  = error "Include units"
+area diam
     |diam <=0   = error "Pipe diameter MUST be greater than 0"
-    |otherwise  = (pi/4) * (diam*factor)^2
-    where
-        factor  = unitsCheck (units)
+    |otherwise  = (pi/4) * diam^2
 
-velocity :: (RealFloat a) => ((a, String) , (a, String) ) -> a
-velocity ((flow, flowU), (diam, diamU))
+velocity :: (RealFloat a) => (a, a) -> a
+velocity (flow, diam)
     |flow <=0   = error "Flow MUST bre greater than 0"
     |diam <=0   = error "Pipe diameter MUST be greater than 0"
-    |otherwise  = (flow*factorU)/(area (diam, diamU))
-    where
-        factorU = unitsCheck (units)
-
--- dispatcher 
+    |otherwise  = flow $ area diam
