5eb0a6845f505e5b29bd5c72531d5dd3f396fdd3
Lijantropique <lijantropique@protonmail.com>
2017-08-22 14:57:48 -0500

    Add more examples data types

--- Exercises/Chapter8.hs
+++ Exercises/Chapter8.hs
@@ -8,6 +8,8 @@ module Chapter8
     , Person(..)
     ) where
 
+import qualified Data.Map as Map
+
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
@@ -30,3 +32,68 @@ data Person = Person    {fisrtName :: String
                         ,phoneNumber :: String
                         ,flavor:: String
                         }deriving (Show)
+
+
+-- type parameters
+data Vector a = Vector a a a deriving (Show)
+
+vplus:: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+-- Based on the input, it would a a type Vector int, Vector Integer, Vector Float etc
+
+
+-- Derived instances
+data Day = Monday | Tuesday | Wednesday | Thrusday
+    deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+data Unit =  Unit {name::String, factor::Double} deriving (Show, Read, Ord, Eq)
+data U = U {val::Double, unit::Unit} deriving (Show)
+
+mm' = Unit "mm" 0.001
+ft' = Unit "ft" 0.3048
+in' = Unit "ft" (0.3048/12)
+gpm' = Unit "gpm" 6.309E-5
+
+lengthU = [mm',ft',in']
+flowU = [gpm']
+
+areaM :: U -> Double
+areaM (U v u)
+    |u `elem` lengthU = (pi/4) * (v* factor u)^2
+    |otherwise = error "Units not valid for function"
+
+-- funUnits ::
+parseUnits (U v u) group
+    |u `elem` group = v * factor u
+    |otherwise =  error "Units not valid for function"
+
+areaM' (U v u) =  (pi/4) * v'^2
+    where
+        v' = parseUnits (U v u) lengthU
+
+velM flow diam=  f'/ areaM' diam
+    where
+        f' = parseUnits flow flowU
+
+velM' flow diam=  f'/ ((pi/4) * v'^2)
+    where
+        f' = parseUnits flow flowU
+        v' = parseUnits diam lengthU
+
+data Pipe = Pipe { di::U, material::String} deriving (Show)
+
+newPipe :: Double -> Unit -> String -> Pipe
+newPipe di' u' m'
+    |u' `elem` lengthU = Pipe { di=(U di' u'), material=m'}
+    |otherwise = error "Units not valid"
+    -- map (\(x, g) -> x `elem` g) $ zip [u1, u2] [g1, g2]
+    -- all (==True) $ map (\(x,g)->x `elem` g) $ zip [1,2,3] [[1,2,3],[4,5,6,2],[3,4,6]]
+
+areaM'' (Pipe di _) = areaM' di
+
+
+data Foo = Type1 | Type2 deriving (Show)
+a = Type1
+
+plus :: (Num a) => Foo -> a ->a
+plus f a = 1+ a
