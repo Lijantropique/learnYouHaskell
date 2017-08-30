68702769b3f8eaa89fdc2b2d89ed64ea054934e3
Lijantropique <lijantropique@protonmail.com>
2017-08-30 14:58:44 -0500

    Move Engineering folder to own repo

--- Engineering/eng2.hs
+++
@@ -1,7 +0,0 @@
-{- ==========================
-Project: Process Engineering Calculations with Haskell
-========================== -}
-import qualified Hydraulics as H
-p = H.newPipe 0.1023 30.48 4.572E-5 "ss"
-f = H.newFluid 0.0126 1000 0.001 "water"
-dp =  H.pressureDrop f p
