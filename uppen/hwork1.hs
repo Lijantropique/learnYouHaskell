29b06452a7bd0b306a43a5065a69f29e0b5e1b1c
Lijantropique <lijantropique@protonmail.com>
2017-08-31 10:07:21 -0500

    Add solutions for HW upenn

--- uppen/hwork1.hs
+++ uppen/hwork1.hs
@@ -39,3 +39,20 @@ validate n
     |otherwise                      = False
     where
         nTransformed = sumDigits $ doubleEveryOther $ toDigits n
+
+-- ===| Exercise 5 |=== --
+--  The Towers of Hanoi
+type Peg = String
+type Move = (Peg, Peg)
+hannoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hannoi 1 a b _ = [(a, b)]
+hannoi n a b c = hannoi (n-1) a c b ++ [(a, b)] ++ hannoi (n-1) c b a
+
+-- ===| Exercise 6 |=== --
+--  The Towers of Hanoi, 4 pegs
+hannoi4P :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hannoi4P 1 a b _ _ = [(a, b)]
+hannoi4P 2 a b c _ = [(a, c), (a, b), (c, b)]
+hannoi4P 3 a b c d = [(a, c), (a, d), (a, b), (d,b),(c,b) ]
+-- hannoi4P n a b c d = hannoi4P (n-2) a c b d ++ [(a, d), (a, b), (d, b)] ++ hannoi4P (n-2) c b a d
+hannoi4P n a b c d = hannoi4P (n-2) a c b d ++ hannoi4P 2 a b d c ++ hannoi4P (n-2) c b a d
